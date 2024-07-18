82fd195c56f19e72966582a71ae8bac518926ee0
Eui Joong Kim <ejk5148@gmail.com>
2024-07-18 22:00:47 +0900

    Upload: kimdevspace / 240718 / [SWEA_VIEW_1206] / 132ms

--- EuiJoong/WeeK01/SWEA_VIEW.java
+++ EuiJoong/WeeK01/SWEA_VIEW.java
@@ -18,16 +18,16 @@ public class Main {
             }
 
             int ans = 0;
-
+            //앞뒤 두영역 제외하고 탐색
             for (int i = 2; i < buildingHeight.length - 2; i++) {
-                int tempArr[] = new int[5];
+                int tempArr[] = new int[5]; // 앞두칸 뒤두칸 이므로 크기 5의 새로운 배열 생성
                 int tempIndex = 0;
                 for (int j = i - 2; j < i + 3; j++) {
-                   tempArr[tempIndex++] = buildingHeight[j];
+                   tempArr[tempIndex++] = buildingHeight[j]; //새로운 배열에 앞 두칸부터 뒤 두칸까지 요소 담아서
                 }
-                Arrays.sort(tempArr);
-                if (buildingHeight[i] == tempArr[4]) {
-                    ans += tempArr[4] - tempArr[3];
+                Arrays.sort(tempArr); // 정렬
+                if (buildingHeight[i] == tempArr[4]) { //만약 현재 탐색 값과 범위 안의 값 중 최댓값이 같다면
+                    ans += tempArr[4] - tempArr[3]; // 최댓값 - 그 다음 값을 ans에 누산!
                 }
             }
             bw.write("#" + tc + " " + ans + "\n");
