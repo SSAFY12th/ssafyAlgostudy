0f92e5e2913c178c2a6f78fc806b4278864176e1
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-07-25 20:17:27 +0900

    Upload : 김의중 / 240725 / [SWEA_간단한소인수분해_1945] / 110ms

---
+++ EuiJoong/WeeK01/SWEA_간단한 소인수분해_1945.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[12];
+            int K = 2;
+
+            while (N != 1) {
+                if (N % K == 0) {
+                    arr[K] += 1;
+                    N = N / K;
+                }
+                else {
+                    K++;
+                }
+            }
+            System.out.println("#" + tc + " " + arr[2] + " " + arr[3] + " " + arr[5] + " " + arr[7] + " " + arr[11]);
+        }
+    }
+}
