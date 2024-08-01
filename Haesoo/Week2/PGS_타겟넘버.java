c0a06e43cf4aac36400dc3598795f921ca1a7d98
haesoo <98267078+haesoooo@users.noreply.github.com>
2024-08-01 21:11:12 +0900

    이해수 / 240801 / [PGS_타겟넘버] / 7.3ms

---
+++ Haesoo/Week2/PGS_타겟넘버.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    static int answer = 0;
+    public void dfs (int [] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length) {
+            if (sum == target) answer++;
+            return;
+        }
+        dfs (numbers, target, sum + numbers[depth], depth + 1);
+        dfs (numbers, target, sum - numbers[depth], depth + 1);
+    }
+    public int solution(int[] numbers, int target) {
+        dfs (numbers, target, 0, 0);
+        return answer;
+    }
+}
