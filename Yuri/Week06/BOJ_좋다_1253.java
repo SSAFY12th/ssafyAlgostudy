a1069d66b0ee1448ec8a9c04b6734374654f7684
yuri <mesmgr01@gmail.com>
2024-08-28 11:25:43 +0900

    이유리 / 240828 / [BOJ_좋다_1253] / 152ms / 14,756 kb

---
+++ Yuri/Week06/BOJ_좋다_1253.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static long num[];
+    static long result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        num = new long[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++)
+            num[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(num);
+
+        // 음수도 입력될 수 있음.
+        int s, e;
+        long sum;
+        for (int i = 0; i < n; i++) {
+            s = 0;
+            e = n-1;
+
+            if(s == i) s++;
+            if(e == i) e--;
+
+            while(s < e) {
+                sum = num[s]+num[e];
+                if(sum == num[i]) {
+                    if(s != i && e != i) {
+                        result++;
+                        break;
+                    }
+                    else if(s == i) s++;
+                    else e--;
+                }
+                else if(sum > num[i]) {
+                    e--;
+                }
+                else {
+                    s++;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
