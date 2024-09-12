a662a7d4fb83a9c88ccfd0f8f4a79686c49482ec
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-09-12 18:42:51 +0900

    Update BOJ_파이프옮기기1_17070.java

--- EuiJoong/Week08/BOJ_파이프옮기기1_17070.java
+++ EuiJoong/Week08/BOJ_파이프옮기기1_17070.java
@@ -1,93 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int T;
-    static int N;
-    static List<List<Integer>> adjList = new ArrayList<>();
-    static ArrayList<Integer> result;
-    static int[] inDegree;
-    static int ans;
+	static int N;
+	static int[][] map;
+	static int ans;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            inDegree = new int[N + 1];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            for (int i = 0; i <= N; i++) {
-                adjList.add(new ArrayList<>());
-            }
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dfs(0, 1, 0);
+		System.out.println(ans);
+	}
 
-            for (int i = 1; i <= N; i++) {
-                st = new StringTokenizer(br.readLine());
-                int adjCnt = Integer.parseInt(st.nextToken());
-                if (adjCnt > 0) {
-                    for (int j = 0; j < adjCnt; j++) {
-                        int b = Integer.parseInt(st.nextToken());
-                        adjList.get(b).add(i);
-                        inDegree[i] += 1;
-                    }
-                }
-            }
-            //System.out.println(Arrays.toString(inDegree));
-            //System.out.println(adjList);
-            ans = -1;
-            int tmp = 0;
-            result = new ArrayList<>();
-            topologySort();
+	private static void dfs(int r, int c, int dir) {
 
-            for (int i = 1; i <= N; i++) {
-                System.out.println(bfs(i));
-            }
-        }
-    }
+		if (r == N - 1 && c == N - 1) {
+			ans++;
+			return;
+		}
 
-    private static void topologySort() {
-        Queue<Integer> q = new LinkedList<>();
+		//우
+		if (dir == 0) {
+			if (chkRight(r, c)) dfs(r, c + 1, 0);
+			if (chkRightDown(r, c)) dfs(r + 1, c + 1, 2);
+		}
+		//하
+		if (dir == 1) {
+			if (chkDown(r, c)) dfs(r + 1, c, 1);
+			if (chkRightDown(r, c)) dfs(r + 1, c + 1, 2);
+		}
+		//우하
+		if (dir == 2) {
+			if (chkRight(r, c)) dfs(r, c + 1, 0);
+			if (chkDown(r, c)) dfs(r + 1, c, 1);
+			if (chkRightDown(r, c)) dfs(r + 1, c + 1, 2);
+		}
 
-        // 처음 시작할 떄는 진입차수가 0 인 노드를 큐에 삽입
-        for (int i = 1; i <= N; i++) {
-            if (inDegree[i] == 0) {
-                q.offer(i);
-            }
-        }
 
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            result.add(cur);
-            for (int i = 0; i < adjList.get(cur).size(); i++) {
-                inDegree[adjList.get(cur).get(i)] -= 1;
-                if (inDegree[adjList.get(cur).get(i)] == 0) {
-                    q.offer(adjList.get(cur).get(i));
-                }
-            }
-        }
-    }
 
-    private static int bfs(int v) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visit = new boolean[N + 1];
-        q.offer(v);
-        visit[v] = true;
-        int cnt = 1;
 
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int i = 0; i < adjList.get(cur).size(); i++) {
-                int w = adjList.get(cur).get(i);
-                if (!visit[w]) {
-                    q.offer(w);
-                    cnt++;
-                    visit[w] = true;
-                }
-            }
-        }
-        return cnt;
-    }
+
+
+	}
+	private static boolean chkRight(int r, int c) {
+		if (c + 1 < N && map[r][c + 1] != 1) return true;
+		return false;
+	}
+	private static boolean chkDown(int r, int c) {
+		if (r + 1 < N && map[r + 1][c] != 1) return true;
+		return false;
+	}
+	private static boolean chkRightDown(int r, int c) {
+		if (r + 1 < N && c + 1 < N && map[r + 1][c] != 1 && map[r][c + 1] != 1 && map[r + 1][c + 1] != 1) {
+			return true;
+		}
+		return false;
+	}
 }
