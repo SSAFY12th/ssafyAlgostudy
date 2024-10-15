26e2115765a4860d265c7da9009ee9f332b633c8
yuri <mesmgr01@gmail.com>
2024-10-15 14:36:45 +0900

    이유리 / 241015 / [PGS_퍼즐게임챌린지] / 테스트 16 (56.11ms, 130MB)

---
+++ Yuri/Week13/PGS_퍼즐게임챌린지.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int solution(int[] diffs, int[] times, long limit) {
+        int left = 1;
+        int right = 0;
+        for (int diff : diffs) {
+            right = Math.max(right, diff);
+        }
+        int mid = left;
+        int time_prev = 0;
+        int answer = 0;
+        while(left <= right) {
+            mid = (right+left)/2;
+
+            long time = 0;
+            for (int i = 0; i < diffs.length; i++) {
+                if(diffs[i] <= mid) {  // 내 숙련도로 풀이 가능한 경우
+                    time += times[i];
+                } else {              // 내 숙련도로 풀이 불가능한 경우
+                    time += (diffs[i] - mid) * time_prev + times[i] * (diffs[i] - mid);
+                    time += times[i];
+                }
+                time_prev = times[i];
+            }
+
+            if(time <= limit) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        answer = left;
+        return answer;
+    }
+}
