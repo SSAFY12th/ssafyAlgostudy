4375750251c389bab16ad0e47b90196d80afa3f8
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-10-31 11:52:09 +0900

    김의중 / 241031 / BOJ_문자열폭발_9935 / 368ms / 82,424kb

---
+++ EuiJoong/Week15/BOJ_문자열 폭발_9935.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String crushStr = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            // 문자열 순회하며 스택에 문자 하나씩 추가
+            sb.append(str.charAt(i));
+            // 폭발 문자열 검사
+            if (sb.length() >= crushStr.length()) {
+                String subString = sb.substring(sb.length() - crushStr.length());
+
+                if (subString.equals(crushStr)) {
+//                    System.out.println(subString);
+                    sb.delete(sb.length() - crushStr.length(), sb.length());
+                }
+            }
+        }
+
+        if (sb.length() == 0) {
+            System.out.println("FRULA");
+        } else {
+            System.out.println(sb);
+        }
+
+    }
+}
